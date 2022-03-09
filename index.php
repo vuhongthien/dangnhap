69e3bc1f9937b053ab8daa08bff37aedce33f9a4
DevThien <tn789237@gmail.com>
2022-03-09 09:34:22 +0700

    tao user

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+<div id="wrapper">
+    <form action="xulylogin.php" method="Post"  id="form-login">
+        <h1 class="form-heading">Đăng Nhập</h1>
+        <div class="form-group">
+            <i class="far fa-user"></i>
+            <label>UserName</label>
+            <input type="text" class="form-input" name="username" id="txtUserName" placeholder="Tên đăng nhập">
+        </div>
+        
+    </form>
+</div>
+</body>
+</html>
