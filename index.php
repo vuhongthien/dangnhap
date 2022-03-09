81c497df460763628a4cf5b157c5475ec7e6db1f
DevThien <tn789237@gmail.com>
2022-03-09 09:38:19 +0700

    tao login

--- index.php
+++ index.php
@@ -18,10 +18,10 @@
 
         <div class="form-group">
             <i class="far fa-key"></i>
-            <label>Mật khẩu</label>
-            <input type="password" class="form-input" name="password" id="txtmatkhau" placeholder="Mật khẩu">
+            <label>Password</label>
+            <input type="password" class="form-input" name="password" id="txtpassword" placeholder="Mật khẩu">
         </div>
-        
+        <input type="submit" name="submit" value="btnlogin" class="form-submit">
     </form>
 </div>
 </body>
